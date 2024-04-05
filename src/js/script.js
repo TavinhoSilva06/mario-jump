3ace57d341ffe9c39c6ff693fe457756f76ec687
TavinhoSilva06 <tavinho.augusto.amaral@gmail.com>
2024-04-05 11:39:59 -0300

    Entendendo o que é as funções do touch no mario-jump

--- src/js/script.js
+++ src/js/script.js
@@ -40,10 +40,15 @@ document.addEventListener('keydown', pulo);
 
 function startup() {
     console.log("Entrou no mario");
-
+//Chama um elemento "document" que seria todo o conteúdo do meu HTML, 
+//e chama a função "querySelector", na qual pega um elemento que contém a classe ("game-board")
+//E atribui o que foi encontrado a uma variável chamda "el"
     var el = document.querySelector(".game-board");
     console.log("Valor da variavel el", el);
+//adiciona a variável "el", logo chamando a função "addEventListener", na qual nomeia o nome "touchstart"
+//e chama outra função chamada "tocounatela", o que com tudo isso, o jogo vai reconhecer o toque do dedo
     el.addEventListener("touchstart", tocounatela, false);
+
 
 }
 
