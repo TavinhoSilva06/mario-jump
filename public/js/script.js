5921e98cc9bac035ced31dca6a9dcf6e8d69c384
TavinhoSilva06 <tavinho.augusto.amaral@gmail.com>
2024-04-05 11:26:54 -0300

    Mario-jump completo para pc e mobile

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,30 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
-const jump = () => {
-    mario.classList.add('jump');
+const pulo = () => {
+    mario.classList.add('jump-class');
 
     setTimeout(() => {
-        
-        mario.classList.remove('jump');
+
+        mario.classList.remove('jump-class');
 
     }, 500);
 }
 
 const loop = setInterval(() => {
     //Essa linha pega a posicao atual do mario//
-    console.log('loop'+mario.offsetLeft)
+    console.log('loop' + mario.offsetLeft)
 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-        
+
         pipe.style.animation = 'none';
-        pipe.style.left = '${pipePosition}px' ;
+        pipe.style.left = '${pipePosition}px';
 
         mario.style.animation = 'none';
-        mario.style.left = '${marioPosition}px' ;
+        mario.style.left = '${marioPosition}px';
 
         mario.src = './images/game-over.png';
         mario.style.width = '75px'
@@ -35,5 +35,21 @@ const loop = setInterval(() => {
     }
 
 }, 10);
+//Adiciona o comportamento de pulo, quando o usuário tocar na tecla "espaço" do teclado
+document.addEventListener('keydown', pulo);
 
-document.addEventListener('keydown', jump);
+function startup() {
+    console.log("Entrou no mario");
+
+    var el = document.querySelector(".game-board");
+    console.log("Valor da variavel el", el);
+    el.addEventListener("touchstart", tocounatela, false);
+
+}
+
+function tocounatela(evt) {
+    evt.preventDefault();
+    pulo();
+}
+
+
