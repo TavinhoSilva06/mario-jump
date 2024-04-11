75b082e13ab1b547c70e23bb983be5fe254ec3bc
TavinhoSilva06 <tavinho.augusto.amaral@gmail.com>
2024-04-11 11:18:01 -0300

    adicionando o codigo para recarregar a pagina automaticamente

--- public/js/script.js
+++ public/js/script.js
@@ -12,13 +12,18 @@ const pulo = () => {
 }
 
 const loop = setInterval(() => {
-    //Essa linha pega a posicao atual do mario//
-    console.log('loop' + mario.offsetLeft)
-
+    
+    //Posição atual do pipe
     const pipePosition = pipe.offsetLeft;
+    console.log("posição do pipe", pipePosition)
+   
+    
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    console.log("posição do mario", marioPosition)
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        console.log("Foi de vasco")
 
         pipe.style.animation = 'none';
         pipe.style.left = '${pipePosition}px';
@@ -29,8 +34,15 @@ const loop = setInterval(() => {
         mario.src = './images/game-over.png';
         mario.style.width = '75px'
         mario.style.marginLeft = '50x'
+        
+        //Recarrega a página da URL automaticamente assim que o mario morre.
+        //E tem um intervalo de tempo de 1 segundo para que isso aconteça.
+        setTimeout(function() {
+            window.location.reload(true);
+          }, 1000);
 
         clearInterval(loop);
+
 
     }
 
@@ -40,17 +52,17 @@ document.addEventListener('keydown', pulo);
 
 function startup() {
     console.log("Entrou no mario");
-//Chama um elemento "document" que seria todo o conteúdo do meu HTML, 
-//e chama a função "querySelector", na qual pega um elemento que contém a classe ("game-html")
-//E atribui o que foi encontrado a uma variável chamda "el"
+    //Chama um elemento "document" que seria todo o conteúdo do meu HTML, 
+    //e chama a função "querySelector", na qual pega um elemento que contém a classe ("game-html")
+    //E atribui o que foi encontrado a uma variável chamda "el"
     var el = document.querySelector(".game-html");
     console.log("Valor da variavel el", el);
-//adiciona a variável "el", logo chamando a função "addEventListener", na qual nomeia o nome "touchstart"
-//e chama outra função chamada "tocounatela", o que com tudo isso, o jogo vai reconhecer o toque do dedo
+    //adiciona a variável "el", logo chamando a função "addEventListener", na qual nomeia o nome "touchstart"
+    //e chama outra função chamada "tocounatela", o que com tudo isso, o jogo vai reconhecer o toque do dedo
     el.addEventListener("touchstart", tocounatela, false);
 
-
 }
+
 
 function tocounatela(evt) {
     pulo();
